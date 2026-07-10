49add330548fda39090d86a5079b25dd93c22fbb
Santiago <santiago.pulla702@ist17dejulio.edu.ec>
2026-07-10 07:42:41 -0500

    Tabla paises

---
+++ src/main/java/Controlador/PaisControlador.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+import com.mycompany.practicaarquitecturas.modelo.Pais;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import controlador.ConexionBDD;
+/**
+ *
+ * @author SUPERTRONICA
+ */
+public class PaisControlador {
+   
+    //INSTANCIAR LA CONEXIÓN A LA BASE DE DATOS
+    ConexionBDD conectar = new ConexionBDD();
+    //CLASE QUE ME PERMITA CONECTARME DIRECTAMENTE A MYSQL
+    Connection conectado = (Connection) conectar.conectar();
+    //CLASE QUE ME PERMITE EJECUTAR MI SENTENCIA SQL
+    PreparedStatement ejecutar;
+    //OBTENER RESULTADOS DE LA CONSULTA, PARECIDA A LAS LISTAS
+    ResultSet resultado;
+
+    //MÉTODOS DE TRANSACCIONABILIDAD
+    public void insertarPais(Pais p) {
+        //1.- UTILIZAR EXCEPCIÓN
+        try {//LANZAR TESTEAR UN CONJUNTO DE CÓDIGO 
+            String sentenciaSQL = "INSERT INTO pais(nombre,capital)values "
+                    + "('" + p.getNombre() + "','" + p.getCapital() + "');";
+            ejecutar = conectado.prepareCall(sentenciaSQL);
+            //TODA INSERCIÓN DEVUELVE UN ESTADO >0 CUANDO FUE FAVORABLE Y MENOR A O CUANDO NO SE REALIZÓ 
+            int res = ejecutar.executeUpdate();
+            if (res > 0) {
+                JOptionPane.showMessageDialog(null, 
+                        "País Creado con éxito");
+                ejecutar.close();
+            } else {
+                JOptionPane.showMessageDialog(null, 
+                        "El País no ha sido creado,"
+                        + " revise que los datos ingresados sean correctos");
+            }
+            conectado.close();
+
+        } catch (SQLException e) {
+            //CAPTURAR PARA DARLE UN TRATAMIENTO 
+            JOptionPane.showMessageDialog(null, 
+                    "Comuniquese con el Administrador para solicitar ayuda");
+            System.out.println("---------------" + e);
+        }
+        
+    }
+    
+    
+    
+}
---
+++ src/main/java/Vista/Main.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+
+import Controlador.PaisControlador;
+import com.mycompany.practicaarquitecturas.modelo.Pais;
+import javax.swing.JOptionPane;
+import controlador.ConexionBDD;
+
+/**
+ *
+ * @author SUPERTRONICA
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //objeto pais"
+          Pais p = new Pais();
+          p.setNombre(JOptionPane.showInputDialog("Ingrese el nombre del pais: "));
+          p.setCapital(JOptionPane.showInputDialog("Ingrese el nombre de la capital: "));
+          
+          PaisControlador p1 = new PaisControlador();
+          p1.insertarPais(p);
+//
+//        //objeto de direecion
+//        Direccion d = new Direccion();
+//        d.setCalle("Bolivar");
+//        d.setCiudad("Ibarra");
+//        d.setCodPostal(100100);
+//        d.setPais(p);
+//        System.out.print(d.toString());
+        
+        
+         //Estudiante
+//        Estudiante e = new Estudiante();
+//        e.setNombre(JOptionPane.showInputDialog("Ingrese sus nombres: "));
+//        e.setApellido(JOptionPane.showInputDialog("Ingrese sus apellidos: "));
+//        String cedulaInput = JOptionPane.showInputDialog("Ingrese su número de cédula:");
+//        if (e.validarCedula(cedulaInput)) {
+//            System.out.println("Cédula válida");
+//            e.setCedula(Integer.parseInt(cedulaInput)); 
+//        } else {
+//            JOptionPane.showMessageDialog(null, "Cédula inválida.", "Error", JOptionPane.ERROR_MESSAGE);
+//            return;
+//        }
+//        
+//        Direccion dir = new Direccion();
+//        dir.setCalle(JOptionPane.showInputDialog("Ingrese su direccion: "));
+//        dir.setCiudad(JOptionPane.showInputDialog("Ingrese la ciudad: "));
+//        dir.setCodPostal(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el codigo postal: ")));
+//        dir.setPais(p);
+//        e.setDireccion(dir);
+//        JOptionPane.showMessageDialog(null, e.toString());
+//        System.out.println(e.toString());
+//        System.exit(0);
+//                
+
+        
+        // Cedula 
+//        String cedula = JOptionPane.showInputDialog("Ingrese la cédula del estudiante:");
+//
+//        if (e.validarCedula(cedula)) {
+//        System.out.println("Cédula válida");
+//        JOptionPane.showMessageDialog(null, "¡La cédula es válida!");
+//        }
+//        else {
+//        System.out.println("Cédula inválida");
+//        JOptionPane.showMessageDialog(null, "Cédula inválida.");
+        
+        
+        ConexionBDD prueba = new ConexionBDD();
+        prueba.conectar();
+        
+        
+        }  
+        
+        
+        
+    }
+    
+    
+   
+    
+
